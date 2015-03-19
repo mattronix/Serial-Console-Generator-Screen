3a3010a833e7f5b2e29fa8d4fe5f9b50dcde9b8d
Matthew Frost <m.frost@mattronix.nl>
2015-03-19 10:29:57 +0100

    1 varible to set log location

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Serial-Console-Generator-Screen
+how to: 
+
+./serial.sh 1 9600 Customer Host LogfileName
--- serial.sh
+++ serial.sh
@@ -3,9 +3,9 @@
 port="$1"
 speed="$2"
 customer="$3"
-filer="$4"
+host="$4"
 project="$5"
-dir="/home/matthew/Seafile/Proact/serial/$customer/$filer"
+dir="/home/user/seriallogs/$customer/$host"
 
 mkdir  -p $dir
 cd $dir
@@ -15,7 +15,7 @@ temp=`mktemp` || exit 1
 echo $temp
 cat << EOF > $temp
 log on
-logfile /home/matthew/Seafile/Proact/serial/$customer/$filer/$project
+logfile $dir/$project
 EOF
 
 sudo screen -c $temp -L  -S $filer  /dev/ttyUSB$port $speed
